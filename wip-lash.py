58e5ff701a4ccf7d0c4a370903fdd356546f2744
regalcat <srbcat@gmail.com>
2017-04-28 18:46:14 -0500

    Redo with scapy and threading
    I redid the program using the sniff command in a separate thread.
    Haven't tested it since my last changes. YMMV

--- wip-lash.py
+++ wip-lash.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-import socket
+import threading
+import re
 import argparse
-import netifaces
-import os
 import curses
-import struct
+from scapy.all import *
 
 #-----------------
 #- WIP-lash v0.1 -
@@ -16,6 +15,8 @@ import struct
 
 # Global defaults
 verbose=False
+counter=0
+nicelist=[]
 
 # This function does all the setup and contains the main loop at the moment
 def main():
@@ -23,7 +24,7 @@ def main():
     parser.add_argument("interface",help="The interface the program should use")
     # TODO: Actually do curses things when this is set
     #parser.add_argument("-i","--interactive",help="Turn on interactive mode and start the GUI")
-    #parser.add_argument("-n","--network",help="The network to be monitored")
+    parser.add_argument("-n","--network",help="The BSSID of the network to be monitored")
     parser.add_argument("-v","--verbose",help="Sets the verbosity to 'loud'")
     arguments = parser.parse_args()
     
@@ -31,103 +32,49 @@ def main():
         global verbose
         verbose=True
 
+    if arguments.network:
+        global nicelist
+        nicelist.append(arguments.network)
+
     print("Starting packet capture on interface {}".format(arguments.interface))
-    # Create the raw socket
-    try:
-        iface = socket.socket(socket.AF_LINK,socket.SOCK_RAW)
-        mac = get_mac(arguments.interface)
-        # Ask for the networks to be monitored
-        #try:
-        #    networks = os.system("iwlist " + args.interface + " scan");
-        #    print(getnets(networks))
-        #except:
-        #    print("Could not get network list. Interface doesn't support scanning.")
-        nets = input("Which networks would you like to monitor?: ")
-        nets = nets.split()
-        iface.bind(mac,0)
+    sniffer_args = {
+                interface = arguments.interface
+            }
+    sniffer = SnifferThread(1,"sniffer1",sniffer_args)
+    sniffer.start()
+    quit=False
+    while not quit:
+        cmd = input("Enter a command or help for a list of commands: \n")
+        if cmd.lower()=="quit":
+            print("Exiting normally")
+            sys.exit(0)
+        elif cmd.lower()=="help":
+            print("Command - Description")
+            print("quit ..... Terminates the program")
+            print("help ..... Prints this list of commands")
 
-        print("Initiating wireless intrusion detection on interface {}".format(arguments.interface))
-        # Do the 'D' and 'P' parts of WIDS and WIPS here
-        # Might want multiple threads here in the future
-        # Especially if the monitor function takes a while
-        quit = False
-        while(not quit):
-            # Receive the packet
-            packet = iface.recvfrom(65565)
-            # Process the packet
-            if(args.network):
-                verdict = monitor(packet)
-            else:
-                verdict = monitor(packet, args.v)
-            # Taste the packet
-            if verdict[0]:
-                print("A deauth was detected.")
-                print("Reason: {}".format(verdict[1]))
-                print("Flags: {}".format(verdict[2]))
-                print("Checksum: {}".format(verdict[3]))
-                print("Timestamp (UTC): {}".format(verdict[4]))
-                # If the 
+        elif cmd.lower()==re.match('',cmd.lower())
 
 
-    except:
-        print("Cannot create socket")
+class SnifferThread(threading.Thread):
+    def __init__(self, threadId, name, kargs):
+        threading.Thread.__init__(self)
+        self.threadId = threadId
+        self.name = name
+        self.iface = kargs.interface
 
+    def run(self):
+        print("Sniffer thread is running")
+        sniff(iface=self.iface, prn=PacketHandler)
 
-# Gets the MAC address of the specified interface
-def get_mac(ifname):
-    addrs = netifaces.ifaddresses(ifname)
-    macs = addrs[netifaces.AF_LINK]
-    return macs[0].get('addr')
+def PacketHandler(pkt):
+    global counter
+    counter+=1
+    if pkt.haslayer(Dot11):
+        if pkt.type==0 and pkt.subtype==12:
+            if pkt.flags==0 or verbose: #TODO check the checksum
+                print("Deauth packet detected: %s -> %s Flags: %s" %(pkt.addr1,pkt.addr2,pkt.flags))
 
-# Parses a list of APs with info and returns a list of tuples with the name and 
-# other useful info
-#def getnets(net_dict):
-    # TODO
-
-# Detects deauth packets
-# Inputs: pkt: the packet to be looked at
-#    net: the MAC address of the network we want to monitor
-# Returns a tuple
-def monitor(packet, net=None):
-    # Get the timestamp
-    timestamp = time.now()
-    # Chop the packet into sushi
-    pkt = packet[0]
-    radiotap = pkt[0:25]
-    ieee80211 = pkt[26:49]
-    data = pkt[50:]
-    wifihdr = unpack('!BBH6s6s6sH',ieee80211)
-    # Check if the packet-sushi is not a deauth packet
-    typeSubtype = wifihdr[0]
-    subtype = typeSubtype >> 4
-    if not (subtype==12):
-        return [False]
-    # Check if it's aimed at our network
-    
-    if net!=None:
-        return [False]
-    
-    sushi = unpack('!HI',data)
-    reason = sushi[0]
-    chksum = sushi[1]
-
-    if reason==1:
-        reason = "Unspecified reason"
-    if reason==2:
-        reason = "Previous authentication no longer valid (Severity: Note?)"
-    if reason==3:
-        reason = "Sending STA is leaving or has left (Severity: Note?)"
-    if reason==6:
-        reason = "Class 2 frame from nonassociated STA"
-    if reason==7:
-        reason = "Class 3 frame from nonassociated STA"
-    if reason==10:
-        reason = "Info in the Power Capability element is unacceptable (Severity: Note?)"
-    # TODO - Add 'or checksum is bad'
-    if(verbose or flags==0x00):
-        return [True, reason, flags, chksum, timestamp]
-    # Or if it's a disassociate
-    
 
 if __name__ == '__main__':
     main()
