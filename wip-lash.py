803729f9159fa49616aece26022f009cb4593c33
regalcat <srbcat@gmail.com>
2017-04-29 19:18:48 -0500

    Add tracking of routers via signal strength

--- wip-lash.py
+++ wip-lash.py
@@ -17,14 +17,12 @@ from scapy.all import *
 verbose=False
 counter=0
 nicelist=[]
-# Just made of concurrency issues
-naughtylist=[]
-naughtydicts=[]
 
 # This function does all the setup and contains the main loop at the moment
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("interface",help="The interface the program should use")
+    parser.add_argument("mode",help="The mode to use (deauth and tracker are currently supported)")
     # TODO: Actually do curses things when this is set
     #parser.add_argument("-i","--interactive",help="Turn on interactive mode and start the GUI")
     parser.add_argument("-n","--network",help="The BSSID of the network to be monitored")
@@ -42,6 +40,7 @@ def main():
     print("Starting packet capture on interface {}".format(arguments.interface))
     sniffer_args = {
             'interface' : arguments.interface
+            'mode' : arguments.mode.lower()
             }
     sniffer = SnifferThread(1,"sniffer1",sniffer_args)
     sniffer.start()
@@ -57,10 +56,10 @@ def main():
             print("Command - Description")
             print("quit .......... Terminates the program")
             print("help .......... Prints this list of commands")
-            print("add [MAC] ..... Adds the MAC to the list of protected networks")
-            print("rm [MAC] ...... Removes the MAC from the list of protected networks")
-            print("list nice ..... Lists the networks being protected")
-            print("list naughty .. Lists MACs that have sent 'bad' deauth packets")
+            #print("add [MAC] ..... Adds the MAC to the list of protected networks")
+            #print("rm [MAC] ...... Removes the MAC from the list of protected networks")
+            #print("list nice ..... Lists the networks being protected")
+            #print("list naughty .. Lists MACs that have sent 'bad' deauth packets")
 
         #elif cmd.lower()==re.match('add \d\d:\d\d:\d\d:\d\d:\d\d:\d\d',cmd.lower()):
             #TODO
@@ -70,12 +69,6 @@ def main():
         #    print("Protecting these networks")
         #    for n in nicelist:
         #        print(n)
-        #elif cmd.lower()=="list naughty":
-        #    print("Gathering intel on these MACs")
-        #    print("MAC | Sig Str | Target | Probes")
-            # Probably a concurrency issue
-        #    for n in naughtydicts:
-        #        print("{} | {} | {} | {}".format(n.mac,n.sigstr,n.targets,n.probes))
         else:
             print("Sorry, I didn't catch that. Please try again.")
 
@@ -86,30 +79,43 @@ class SnifferThread(threading.Thread):
         self.threadId = threadId
         self.name = name
         self.iface = kargs['interface']
+        self.mode = kargs['mode']
 
     def run(self):
         print("Sniffer thread is running")
-        print("Deauth detection initiated!")
-        sniff(iface=self.iface, prn=PacketHandler)
+        if self.mode=="tracker":
+            print("Activating tracking mode!")
+            sniff(iface=self.iface, prn=TrackerHandler)
+        elif self.mode=="deauth":
+            print("Activating deauth detection!")
+            sniff(iface=self.iface, prn=DeauthHandler)
+        else:
+            print("Invalid mode: '{}'".format(self.mode))
 
 # This needs to be as lean as possible
 # Yeah, Steff. That'll happen
-def PacketHandler(pkt):
+# Watches networks on the nice list for deauth packets
+def DeauthHandler(pkt):
     global counter
     counter+=1
     if pkt.haslayer(Dot11):
         if pkt.type==0 and pkt.subtype==12:
             global nicelist
             if (pkt.addr1 in nicelist) or (pkt.addr2 in nicelist) or verbose:
-                # Probably concurrency issues here
-                #global naughtylist
-                #if pkt.addr1.lower() in naughtylist:
-                    
-                #else:
-
                 print("Deauth: {} -> {} Flags: {} Reason: {}".format(pkt.addr1,pkt.addr2,pkt.flags,pkt[Dot11Deauth].reason))
-        #elif pkt.
 
+# Tracker mode looks at the signal strength of the AP's packets to determine whether its
+# identity is being spoofed
+# Assumes that neither the AP nor the device running this code is moving
+# Future applications: 
+def TrackerHandler(pkt):
+    global counter
+    counter+=1
+    if pkt.haslayer(Dot11):
+        if pkt.addr1 in nicelist:
+            print("{}'s signal strength is {}".format(pkt.addr1,pkt[RadioTap].dBm_TX_Power))
+        if pkt.addr2 in nicelist:
+            print("{}'s signal strength is {}".format(pkt.addr2,pkt[RadioTap].dBm_TX_Power))
 
 if __name__ == '__main__':
     main()
